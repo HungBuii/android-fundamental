fe9ef10372854c39ca626bba919fa0d32971d20a
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2024-01-10 18:28:36 +0700

    update 2

--- project/FirstLook/app/src/main/java/com/example/firstlook/MainActivity.java
+++ project/FirstLook/app/src/main/java/com/example/firstlook/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.firstlook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button hiButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.new_layout);
+
+        hiButton = findViewById(R.id.hiButton);
+        hiButton.setText("Hello Button");
+        hiButton.setTextColor(Color.BLUE);
     }
 }

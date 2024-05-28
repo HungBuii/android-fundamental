eb87f7e89398c4a4d270fbce5ca3df02915e18db
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2024-05-28 16:26:27 +0700

    update 8
    remove all PDF
    update FirstLook project

--- project/FirstLook/app/src/main/java/com/gohool/firstlook/firstlook/MainActivity.java
+++ project/FirstLook/app/src/main/java/com/gohool/firstlook/firstlook/MainActivity.java
@@ -1,6 +1,8 @@
 package com.gohool.firstlook.firstlook;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button hiButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.new_layout);
+
+        hiButton = (Button) findViewById(R.id.hiButton);
+        hiButton.setText(R.string.button_name);
+        hiButton.setTextColor(Color.RED);
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }

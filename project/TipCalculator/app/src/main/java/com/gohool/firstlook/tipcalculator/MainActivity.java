bfb84ee228fb87cec7fab4e050a658645bb1aa89
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2024-06-11 11:31:07 +0700

    update 10

--- project/TipCalculator/app/src/main/java/com/gohool/firstlook/tipcalculator/MainActivity.java
+++ project/TipCalculator/app/src/main/java/com/gohool/firstlook/tipcalculator/MainActivity.java
@@ -1,6 +1,12 @@
 package com.gohool.firstlook.tipcalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,84 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText enteredAmount;
+    private SeekBar seekBar;
+    private Button calculateButton;
+    private TextView totalResultTextView;
+    private TextView textViewSeekBar;
+    private TextView totalBillTv;
+    private int seekBarPercentage;
+    private float enteredBillFloat;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        enteredAmount = (EditText) findViewById(R.id.billAmountID);
+        seekBar = (SeekBar) findViewById(R.id.percentageSeekBar);
+        calculateButton = (Button) findViewById(R.id.calculateButton);
+        totalResultTextView = (TextView) findViewById(R.id.resultID);
+        textViewSeekBar = (TextView) findViewById(R.id.textViewSeekBar);
+        totalBillTv = (TextView) findViewById(R.id.totalBillTextView);
+
+        calculateButton.setOnClickListener(this);
+//        calculateButton.setOnClickListener(this::onClick);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textViewSeekBar.setText(String.valueOf(seekBar.getProgress()) + "%");
+//                textViewSeekBar.setText(seekBar.getProgress() + "%");
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                seekBarPercentage = seekBar.getProgress();
+
+
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+
+    @Override
+    public void onClick(View v) {
+        calculate();
+    }
+
+    public void calculate()
+    {
+        float result = 0.0f;
+
+        if (!enteredAmount.getText().toString().equals(""))
+        {
+            enteredBillFloat = Float.parseFloat(enteredAmount.getText().toString());
+            result = enteredBillFloat * seekBarPercentage / 100;
+            totalResultTextView.setText("You will tip be " + String.valueOf(String.format("%.2f", result)) + "$");
+            totalBillTv.setText("Total bill: " + String.valueOf(enteredBillFloat + result) + "$");
+
+        } else {
+            Toast.makeText(MainActivity.this, "Please enter a bill amount!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }

d8adbc65b71bee158694309f163eb2d120bfa90a
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2024-09-14 14:53:36 +0700

    update

---
+++ project/ConstraintLayout_ListView/app/src/main/java/com/gohool/firstlook/constraintlayout_listview/MainActivity.java
@@ -0,0 +1,48 @@
+package com.gohool.firstlook.constraintlayout_listview;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private String[] countries;
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list);
+        countries = getResources().getStringArray(R.array.countries); // get array countries through "string.xml" (R.array.countries)
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String country = parent.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this, "Country: " + country, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

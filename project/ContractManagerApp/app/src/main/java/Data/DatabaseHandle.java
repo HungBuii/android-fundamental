6ae33e1d378275c61aee04c523665d79eb6e9f42
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2024-09-19 17:13:50 +0700

    add data to SQLite

--- project/ContractManagerApp/app/src/main/java/Data/DatabaseHandle.java
+++ project/ContractManagerApp/app/src/main/java/Data/DatabaseHandle.java
@@ -1,11 +1,17 @@
 package Data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Contact;
 import Utils.Util;
 
 public class DatabaseHandle extends SQLiteOpenHelper
@@ -33,5 +39,95 @@ public class DatabaseHandle extends SQLiteOpenHelper
 
         // Create table again
         onCreate(db);
+    }
+
+    /*
+        CRUD operations (Create, Read, Update, Delete)
+     */
+
+    // Add contact
+    public void addContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase(); // write data
+
+        ContentValues value = new ContentValues();
+        value.put(Util.KEY_NAME, contact.getName());
+        value.put(Util.KEY_PHONE_NUMBER, contact.getPhoneNumber());
+
+        // Insert to row
+        db.insert(Util.TABLE_NAME, null, value);
+        db.close(); // close database connection
+    }
+
+    // Get a contact
+    public Contact getContact(int id) {
+        SQLiteDatabase db = this.getReadableDatabase(); // only read data
+
+        Cursor cursor = db.query(Util.TABLE_NAME, new String[] {Util.KEY_ID,
+                                Util.KEY_NAME, Util.KEY_PHONE_NUMBER}, Util.KEY_ID + "=?",
+                                new String[] {String.valueOf(id)}, null, null, null, null);
+
+        /*
+            Cursor: Cursor là một interface cung cấp truy cập đến kết quả của một truy vấn cơ sở dữ liệu.
+                Có thể hình dung nó giống như một con trỏ trỏ đến các hàng dữ liệu trong kết quả truy vấn.
+         */
+
+        /*
+            Thực hiện truy vấn SELECT để lấy dữ liệu contact từ bảng Util.TABLE_NAME.
+
+            Tham số đầu tiên là tên bảng.
+            Tham số thứ hai là mảng các cột cần lấy (id, name, phone_number).
+            Tham số thứ ba là điều kiện WHERE (id=?).
+            Tham số thứ tư là mảng các giá trị thay thế cho dấu ? trong điều kiện WHERE.
+            Các tham số còn lại là groupBy, having, orderBy, limit (đều là null trong trường hợp này).
+
+        */
+
+        if (cursor != null)
+        {
+            cursor.moveToFirst(); // Kiểm tra nếu cursor không phải null (tức là có dữ liệu trả về)
+                                  // và di chuyển con trỏ đến dòng đầu tiên.
+
+        }
+
+        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)),
+                                     cursor.getString(1), cursor.getString(2));
+
+        /*
+            Integer.parseInt(cursor.getString(0)): Chuyển đổi giá trị id từ String sang int.
+            cursor.getString(1): Lấy giá trị name.
+            cursor.getString(2): Lấy giá trị phone_number.
+         */
+
+        return contact;
+    }
+
+    // Get all contacts
+    public List<Contact> getAllContacts()
+    {
+        SQLiteDatabase db = this.getReadableDatabase();
+        List<Contact> contactList = new ArrayList<>();
+
+        // Select all contacts
+        String selectAll = "SELECT * FROM " + Util.TABLE_NAME;
+        Cursor cursor = db.rawQuery(selectAll, null);
+
+        // Loop through our contacts
+        if (cursor.moveToFirst())
+        {
+            do {
+                Contact contact = new Contact();
+                contact.setId(Integer.parseInt(cursor.getString(0)));
+                contact.setName(cursor.getString(1));
+                contact.setPhoneNumber(cursor.getString(2));
+
+                // add contact object to our contact list
+                contactList.add(contact);
+
+
+            } while (cursor.moveToNext());
+        }
+
+        return contactList;
+
     }
 }
--- project/ContractManagerApp/app/src/main/java/com/gohool/firstlook/contractmanagerapp/MainActivity.java
+++ project/ContractManagerApp/app/src/main/java/com/gohool/firstlook/contractmanagerapp/MainActivity.java
@@ -1,12 +1,18 @@
 package com.gohool.firstlook.contractmanagerapp;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
+
+import Data.DatabaseHandle;
+import Model.Contact;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +21,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        DatabaseHandle db = new DatabaseHandle(this);
+
+        // Insert contacts
+        Log.d("Insert: ", "Inserting...");
+        db.addContact(new Contact("Nguyễn Văn A", "0123456789"));
+        db.addContact(new Contact("Nguyễn Văn B", "0123456789"));
+        db.addContact(new Contact("Nguyễn Văn C", "0123456789"));
+        db.addContact(new Contact("Nguyễn Văn D", "0123456789"));
+        db.addContact(new Contact("Nguyễn Văn E", "0123456789"));
+        db.addContact(new Contact("Nguyễn Văn F", "0123456789"));
+        db.addContact(new Contact("Nguyễn Văn G", "0123456789"));
+
+        // Read them back
+        Log.d("Reading: ", "Reading all contacts...");
+        List<Contact> contactList = db.getAllContacts();
+        for (Contact c : contactList) {
+//            String log = "Id: " + c.getId() + ", Name: " + c.getName() + ", Phone: " + c.getPhoneNumber();
+            Log.d("Contact:", "Id: " + c.getId() + ", Name: " + c.getName() + ", Phone: " + c.getPhoneNumber());
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
